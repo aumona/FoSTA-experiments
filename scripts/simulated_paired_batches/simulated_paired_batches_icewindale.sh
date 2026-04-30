da36da7a425f94bcdec39c458735330fe4271b1d
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-04-30 18:39:28 -0400

    added loop for many dropout and noise

---
+++ scripts/simulated_paired_batches/simulated_paired_batches_icewindale.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /home/lizottem/RF-MALI
+source ../.profile
+source /opt/anaconda/anaconda3/etc/profile.d/conda.sh
+conda activate $ENV_FOSTA
+
+timestamp=${1:-$(date +%Y-%m-%d_%H-%M-%S)}
+log_dir=/NOBACKUP/lizottem/fosta/logs/simulated_paired_batches/$timestamp
+# create log directory
+mkdir -p "$log_dir"
+
+
+noise_stds=(0 0.2 0.4 0.6 0.8 1.0)
+dropout_probs=(0 0.2 0.4 0.6 0.8 0.95) 
+# noise_stds=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+# dropout_probs=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) 
+n_components=(2) # 3 10 20)
+
+for noise in "${noise_stds[@]}"; do
+  for dropout in "${dropout_probs[@]}"; do
+    for component in "${n_components[@]}"; do
+      python3 scripts/simulated_paired_batches/simulated_paired_batches.py \
+        --globalmasking 0.5  \
+        --savename "simulated_paired_batches/${timestamp}" \
+        --noise "$noise" \
+        --dropout "$dropout" \
+        --components "$component" \
+          > "$log_dir/noise${noise}_dropout${dropout}_comp${component}.out" \
+          2> "$log_dir/noise${noise}_dropout${dropout}_comp${component}.err"
+    done
+  done
+done
