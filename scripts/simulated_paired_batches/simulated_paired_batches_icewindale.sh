f55061bcdf94c9bfc60c9dd8c259cd955e09bfb1
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-04-30 22:01:04 -0400

    sweep over seeds first, only 1 value of dropout and noise

--- scripts/simulated_paired_batches/simulated_paired_batches_icewindale.sh
+++ scripts/simulated_paired_batches/simulated_paired_batches_icewindale.sh
@@ -11,25 +11,25 @@ log_dir=/NOBACKUP/lizottem/fosta/logs/simulated_paired_batches/$timestamp
 mkdir -p "$log_dir"
 
 
-noise_stds=(0 0.2 0.4 0.6 0.8 1.0)
-dropout_probs=(0 0.2 0.4 0.6 0.8 0.95) 
+# noise_stds=(0 0.2 0.4 0.6 0.8 1.0)
+# dropout_probs=(0 0.2 0.4 0.6 0.8 0.95) 
+noise_stds=(0.5)
+dropout_probs=(0.5) 
 # noise_stds=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
 # dropout_probs=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) 
 # n_components=(2) # 3 10 20)
 seeds=(11784 39041 56089 79121 4386721)
 
-for noise in "${noise_stds[@]}"; do
-  for dropout in "${dropout_probs[@]}"; do
-    for seed in "${seeds[@]}"; do
+for seed in "${seeds[@]}"; do
+  for noise in "${noise_stds[@]}"; do
+    for dropout in "${dropout_probs[@]}"; do
       python3 scripts/simulated_paired_batches/simulated_paired_batches.py \
         --globalmasking 0.5  \
         --savename "simulated_paired_batches/${timestamp}" \
         --noise "$noise" \
         --dropout "$dropout" \
-        --seed "$seed" \
-        # --components "$component" \
-          > "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.out" \
-          2> "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.err"
+        --seed "$seed"  \
+       > "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.log" 2>&1
     done
   done
 done
