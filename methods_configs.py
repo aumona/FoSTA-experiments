f301312b3b95d7c895935aaed6bc1ff16cc6eb9d
aumona <adrien.aumon@umontreal.ca>
2026-04-30 23:58:33 -0400

    FoSTA configs to try

--- methods_configs.py
+++ methods_configs.py
@@ -1,15 +1,33 @@
-methods_params_dict = {"FoSTA kerf t=2": {"method_type": "FoSTA",
+methods_params_dict = {"Old FoSTA kerf t=2": {"method_type": "FoSTA",
                                             "kernel_method": "kerf",
                                             "t": 2},
-                       "FoSTA kerf t=auto": {"method_type": "FoSTA",
+                       "Old FoSTA kerf t=auto": {"method_type": "FoSTA",
                                             "kernel_method": "kerf",
                                             "t": "auto"},
-                        "FoSTA gap t=2": {"method_type": "FoSTA",
+                        "Old FoSTA gap t=2": {"method_type": "FoSTA",
                                         "kernel_method": "gap",
                                         "t": 2},
-                       "FoSTA gap t=auto": {"method_type": "FoSTA",
+                       "Old FoSTA gap t=auto": {"method_type": "FoSTA",
                                             "kernel_method": "gap",
                                             "t": "auto"},
+
+
+
+
+                        "New FoSTA original t=2": {"method_type": "FoSTA",
+                                            "kernel_method": "original",
+                                            "t": 2},
+                        "New FoSTA original t=auto": {"method_type": "FoSTA",
+                                            "kernel_method": "original",
+                                            "t": "auto"},
+                        "New FoSTA gap t=2": {"method_type": "FoSTA",
+                                        "kernel_method": "gap",
+                                        "t": 2},
+                        "New FoSTA gap t=auto": {"method_type": "FoSTA",
+                                            "kernel_method": "gap",
+                                            "t": "auto"},
+
+
                         "Scanorama":{},
                         "LIGER":{},
                         "Harmony":{},
