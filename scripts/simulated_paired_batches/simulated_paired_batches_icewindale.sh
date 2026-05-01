be24b24dada6973af49d1d508be87293479ca579
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-05-01 01:06:49 -0400

    changed global masking to 0.2 in simulated paired batches. also added an initial run with 0.5 0.5

--- scripts/simulated_paired_batches/simulated_paired_batches_icewindale.sh
+++ scripts/simulated_paired_batches/simulated_paired_batches_icewindale.sh
@@ -20,11 +20,26 @@ dropout_probs=(0.5)
 # n_components=(2) # 3 10 20)
 seeds=(11784 39041 56089 79121 4386721)
 
+
+
+# first run 0.5 0.5 with all seeds to get a baseline, then run the rest of the combinations
+for seed in "${seeds[@]}"; do
+  python3 scripts/simulated_paired_batches/simulated_paired_batches.py \
+        --globalmasking 0.2  \
+        --savename "simulated_paired_batches/${timestamp}" \
+        --noise 0.5 \
+        --dropout 0.5 \
+        --seed "$seed"  \
+       > "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.log" 2>&1
+done
+
+
+
 for seed in "${seeds[@]}"; do
   for noise in "${noise_stds[@]}"; do
     for dropout in "${dropout_probs[@]}"; do
       python3 scripts/simulated_paired_batches/simulated_paired_batches.py \
-        --globalmasking 0.5  \
+        --globalmasking 0.2  \
         --savename "simulated_paired_batches/${timestamp}" \
         --noise "$noise" \
         --dropout "$dropout" \
