17565f4b1f3007c32f2287f613c9e363a2deec41
aumona <adrien.aumon@umontreal.ca>
2026-05-01 18:19:01 -0400

    fosta configs to try

--- methods_configs.py
+++ methods_configs.py
@@ -1,31 +1,64 @@
-methods_params_dict = {"Old_FoSTA kerf t=2": {"method_type": "FoSTA",
-                                            "kernel_method": "kerf",
-                                            "t": 2},
-                       "Old_FoSTA kerf t=auto": {"method_type": "FoSTA",
-                                            "kernel_method": "kerf",
-                                            "t": "auto"},
-                        "Old_FoSTA gap t=2": {"method_type": "FoSTA",
+methods_params_dict = {
+    
+                    #     "Old FoSTA kerf t=2": {"method_type": "FoSTA",
+                    #                         "kernel_method": "kerf",
+                    #                         "old_version": True,
+                    #                         "t": 2},
+                    #    "Old FoSTA kerf t=auto": {"method_type": "FoSTA",
+                    #                         "kernel_method": "kerf",
+                    #                         "old_version": True,
+                    #                         "t": "auto"},
+
+
+                        "MALI": {"t:": 'auto'
+                        },
+
+
+                        "Old FoSTA gap t=2": {"method_type": "FoSTA",
                                         "kernel_method": "gap",
+                                        "old_version": True,
                                         "t": 2},
-                       "Old_FoSTA gap t=auto": {"method_type": "FoSTA",
-                                            "kernel_method": "gap",
-                                            "t": "auto"},
-
-
-
-
-                        "FoSTA original t=2": {"method_type": "FoSTA",
-                                            "kernel_method": "original",
-                                            "t": 2},
-                        "FoSTA original t=auto": {"method_type": "FoSTA",
-                                            "kernel_method": "original",
-                                            "t": "auto"},
-                        "FoSTA gap t=2": {"method_type": "FoSTA",
+                        
+                        "Old FoSTA gap t=2 (max-normalized rows)": {"method_type": "FoSTA",
                                         "kernel_method": "gap",
+                                        "max_normalize": True,
+                                        "old_version": True,
                                         "t": 2},
-                        "FoSTA gap t=auto": {"method_type": "FoSTA",
+
+                       "Old FoSTA gap t=auto": {"method_type": "FoSTA",
                                             "kernel_method": "gap",
+                                            "old_version": True,
                                             "t": "auto"},
+                        "Old FoSTA gap t=auto (max-normalized rows)": {"method_type": "FoSTA",
+                                            "kernel_method": "gap",
+                                            "max_normalize": True,
+                                            "old_version": True,
+                                            "t": "auto"},
+
+                                            
+
+                        # "New FoSTA kerf t=2": {"method_type": "FoSTA",
+                        #                     "kernel_method": "kerf",
+                        #                     "old_version": False,
+                        #                     "t": 2},
+                        # "New FoSTA kerf t=auto": {"method_type": "FoSTA",
+                        #                     "kernel_method": "kerf",
+                        #                     "old_version": False,
+                        #                     "t": "auto"},
+
+
+
+                        "New FoSTA gap t=2": {"method_type": "FoSTA",
+                                             "kernel_method": "gap",
+                                             "old_version": False,
+                                             "t": 2},
+                                             
+                        "New FoSTA gap t=auto": {"method_type": "FoSTA",
+                                             "kernel_method": "gap",
+                                             "old_version": False,
+                                             "t": "auto"},
+
+                        
 
 
                         # "Scanorama":{},
