2d4c8b7b7c70f74b0950118ff02af88999fe957f
aumona <adrien.aumon@umontreal.ca>
2026-05-01 01:22:54 -0400

    make my own paths

--- personal_paths.py
+++ personal_paths.py
@@ -1,12 +1,12 @@
 
-# ICEWINDALE -- MYRIAM
-BASE_PATH = "/home/lizottem/RF-MALI"
-LUNG_BATCHES_DATA_PATH = "/home/lizottem/RF-MALI/data/lung_atlas.h5ad"
-IMMUNE_BATCHES_DATA_PATH = "/home/lizottem/RF-MALI/data/Immune_ALL_human.h5ad"
-PAIRED_DATA_PATH = "/NOBACKUP/lizottem/fosta/data"
-UCI_DATA_FOLDER = "/NOBACKUP/lizottem/fosta/data"
+# LOCAL MAC -- ADRIEN
+BASE_PATH = "/Users/aumona/Projects/RF-MALI"
+LUNG_BATCHES_DATA_PATH = "/Users/aumona/Projects/RF-MALI/data_sc/lung_batches.h5ad"
+IMMUNE_BATCHES_DATA_PATH = "/Users/aumona/Projects/RF-MALI/data_sc/Immune_ALL_human.h5ad"
+# PAIRED_DATA_PATH = "/NOBACKUP/lizottem/fosta/data"
+UCI_DATA_FOLDER = "/Users/aumona/Projects/RF-MALI/data_uci"
 # RESULTS_PATH = "/home/lizottem/RF-MALI/results"
-RESULTS_PATH = "/NOBACKUP/lizottem/fosta/results"
+RESULTS_PATH = "/Users/aumona/Projects/RF-MALI/results_sc"
 
 
 ## MILA CLUSTER -- MYRIAM
