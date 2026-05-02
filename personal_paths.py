67ba3faa081dc3016353a3ff956a7b00f2a380bc
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-05-01 22:00:51 -0400

    Revert "make my own paths"
    This reverts commit 176fdef76a18932ab2fd87fc1061bc62dd09a0ef.

--- personal_paths.py
+++ personal_paths.py
@@ -1,12 +1,12 @@
 
-# LOCAL MAC -- ADRIEN
-BASE_PATH = "/Users/aumona/Projects/RF-MALI"
-LUNG_BATCHES_DATA_PATH = "/Users/aumona/Projects/RF-MALI/data_sc/lung_batches.h5ad"
-IMMUNE_BATCHES_DATA_PATH = "/Users/aumona/Projects/RF-MALI/data_sc/Immune_ALL_human.h5ad"
-# PAIRED_DATA_PATH = "/NOBACKUP/lizottem/fosta/data"
-UCI_DATA_FOLDER = "/Users/aumona/Projects/RF-MALI/data_uci"
+# ICEWINDALE -- MYRIAM
+BASE_PATH = "/home/lizottem/RF-MALI"
+LUNG_BATCHES_DATA_PATH = "/home/lizottem/RF-MALI/data/lung_atlas.h5ad"
+IMMUNE_BATCHES_DATA_PATH = "/home/lizottem/RF-MALI/data/Immune_ALL_human.h5ad"
+PAIRED_DATA_PATH = "/NOBACKUP/lizottem/fosta/data"
+UCI_DATA_FOLDER = "/NOBACKUP/lizottem/fosta/data"
 # RESULTS_PATH = "/home/lizottem/RF-MALI/results"
-RESULTS_PATH = "/Users/aumona/Projects/RF-MALI/results_sc"
+RESULTS_PATH = "/NOBACKUP/lizottem/fosta/results"
 
 
 ## MILA CLUSTER -- MYRIAM
