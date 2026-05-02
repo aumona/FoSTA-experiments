4e56163a0909eb0a9b84c44591f53752dea10830
aumona <adrien.aumon@umontreal.ca>
2026-05-01 20:30:53 -0400

    configs to try, including ICML FoSTA if needed

--- methods_configs.py
+++ methods_configs.py
@@ -27,7 +27,6 @@ methods_params_dict = {
 
 
 
-
                         
                         "Old FoSTA gap t=2": {"method_type": "FoSTA",
                                         "kernel_method": "gap",
@@ -39,6 +38,7 @@ methods_params_dict = {
                                         "max_normalize": True,
                                         "old_version": True,
                                         "t": 2},
+
 
                        "Old FoSTA gap t=auto": {"method_type": "FoSTA",
                                             "kernel_method": "gap",
@@ -68,20 +68,22 @@ methods_params_dict = {
                                              "old_version": False,
                                              "t": 2},
                                              
+                                             
                         "New FoSTA gap t=auto": {"method_type": "FoSTA",
                                              "kernel_method": "gap",
                                              "old_version": False,
                                              "t": "auto"},
                         
-                        "New FoSTA kerf t=2": {"method_type": "FoSTA",
-                                             "kernel_method": "kerf",
-                                             "old_version": False,
-                                             "t": 2},
+
+                        # "New FoSTA kerf t=2": {"method_type": "FoSTA",
+                        #                      "kernel_method": "kerf",
+                        #                      "old_version": False,
+                        #                      "t": 2},
                                              
-                        "New FoSTA kerf t=auto": {"method_type": "FoSTA",
-                                             "kernel_method": "kerf",
-                                             "old_version": False,
-                                             "t": "auto"},
+                        # "New FoSTA kerf t=auto": {"method_type": "FoSTA",
+                        #                      "kernel_method": "kerf",
+                        #                      "old_version": False,
+                        #                      "t": "auto"},
 
                         
 
