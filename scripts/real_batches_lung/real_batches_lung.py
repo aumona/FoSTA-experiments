52f28d0e4992207494f1519424fdf560d4dee754
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-05-01 21:59:28 -0400

    Revert "save lung results in datetime subfolder"
    This reverts commit b02713fdc1049361115b63ec7ef05b6b4e005246.

--- scripts/real_batches_lung/real_batches_lung.py
+++ scripts/real_batches_lung/real_batches_lung.py
@@ -9,7 +9,6 @@ import pickle
 import json
 import os
 import matplotlib.pyplot as plt
-from datetime import datetime
 
 import sys, pathlib
 sys.path.insert(0, str(next(p for p in [pathlib.Path.cwd()] + list(pathlib.Path.cwd().parents) if (p/"src").is_dir())))
@@ -39,13 +38,8 @@ args = parser.parse_args()
 batches = [args.batch1, args.batch2]
 
 # set save location 
-# set save location 
-now = datetime.now().strftime("%Y%m%d_%H%M")
 save_path_parent = f"{RESULTS_PATH}/{args.savename}"
-# Move {now} to be the first subfolder after the experiment name
-save_path = f"{save_path_parent}/{now}/{batches[0]}_{batches[1]}/seed_{args.seed}"
-
-os.makedirs(save_path, exist_ok=True)
+save_path = f"{save_path_parent}/{batches[0]}_{batches[1]}/seed_{args.seed}" 
 
 # LOAD DATA 
 adata_full = sc.read(data_path)
