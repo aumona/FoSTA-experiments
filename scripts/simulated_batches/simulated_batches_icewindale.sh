3ffbe85cc3b540b6fda28b0e3458beedb857ec19
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-05-04 18:22:37 -0400

    script for simulated batches

---
+++ scripts/simulated_batches/simulated_batches_icewindale.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd /home/lizottem/RF-MALI
+source ../.profile
+source /opt/anaconda/anaconda3/etc/profile.d/conda.sh
+conda activate $ENV_FOSTA
+
+timestamp=${1:-$(date +%Y-%m-%d_%H-%M-%S)}
+log_dir=/NOBACKUP/lizottem/fosta/logs/simulated_batches/$timestamp
+# create log directory
+mkdir -p "$log_dir"
+
+
+noise_stds=(0.25 0.5 0.75)
+dropout_probs=(0.25 0.5 0.75) 
+# noise_stds=(0 0.2 0.4 0.6 0.8 1.0)
+# dropout_probs=(0 0.2 0.4 0.6 0.8 0.95) 
+# noise_stds=(0.5)
+# dropout_probs=(0.5) 
+# noise_stds=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+# dropout_probs=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9) 
+# n_components=(2) # 3 10 20)
+seeds=(39041 56089 79121)
+# 4386721 11784)
+
+
+
+# first run 0.5 0.5 with all seeds to get a baseline, then run the rest of the combinations
+for seed in "${seeds[@]}"; do
+  python3 scripts/simulated_batches/simulated_batches.py \
+        --globalmasking 0.2  \
+        --savename "simulated_batches/${timestamp}" \
+        --noise 0.5 \
+        --dropout 0.5 \
+        --seed "$seed"  \
+       > "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.log" 2>&1
+done
+
+
+
+for seed in "${seeds[@]}"; do
+  for noise in "${noise_stds[@]}"; do
+    for dropout in "${dropout_probs[@]}"; do
+      python3 scripts/simulated_batches/simulated_batches.py \
+        --globalmasking 0.2  \
+        --savename "simulated_batches/${timestamp}" \
+        --noise "$noise" \
+        --dropout "$dropout" \
+        --seed "$seed"  \
+       > "$log_dir/noise${noise}_dropout${dropout}_seed${seed}.log" 2>&1
+    done
+  done
+done
