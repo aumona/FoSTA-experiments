05b43ca32e268769446b5eb109f47bd5561bc6b3
Myriam Lizotte <myriam.lizotte@umontreal.ca>
2026-05-05 17:38:58 -0400

    add options to methods_params_dict

--- methods_configs_test.py
+++ methods_configs_test.py
@@ -1,5 +1,26 @@
-methods_params_dict = { "Scanorama":{}
-                        # "Harmony":{},
-                        # "KEMArbf": {},
-                        # "KEMAlin": {}
+# methods_params_dict = {"ICML FoSTA t=2": {"method_type": "FoSTA_ICML",
+#                                         "t": 2}
+#                         # "Harmony": {}
+#                     }
+
+
+
+# methods_params_dict = {"FoSTA Spectral mu=0.2": {"method_type": "FoSTA_ICML",
+#                                       "embedder": "spectral",
+#                                        "mu": 0.2},
+#                         "FoSTA Spectral mu=0.5": {"method_type": "FoSTA_ICML",
+#                                                               "embedder": "spectral",
+#                                                                "mu": 0.5},
+#                         "FoSTA Spectral mu=0.8": {"method_type": "FoSTA_ICML",
+#                                                               "embedder": "spectral",
+#                                                                "mu": 0.8}                  
+#                     }
+
+
+methods_params_dict = {"FoSTA Spectral mu=0.95": {"method_type": "FoSTA_ICML",
+                                      "embedder": "spectral",
+                                        "mu": 0.95}, 
+                       "FoSTA Spectral mu=1": {"method_type": "FoSTA_ICML",
+                                      "embedder": "spectral",
+                                        "mu": 1.0},              
                     }
